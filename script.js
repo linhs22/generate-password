6f7dfa516197e39748c3a1d6c9e694429724c15e
Henry Lin <henrylin58@gmail.com>
2019-12-01 23:43:57 -0800

    intial html page and intial javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+
+//DON'T TOUCH THIS CODE! This code is adding click handlers and DOM manipulation to the page.  Edit the generatePassword function and all should work properly.
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
+
+//this function will fire when you click the generate password button on the page.  I've set it to alert "You've clicked a button" and return a password of password for now. Update it to make your password
+function generatePassword() {
+    //YOUR CODE HERE
+    alert("ive been clicked");
+    return "password";
+}
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+    copyBtn.removeAttribute("disabled");
+    copyBtn.focus();
+}
+
+function copyToClipboard() {
+    // BONUS 
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// BONUS EVENT LISTENER
